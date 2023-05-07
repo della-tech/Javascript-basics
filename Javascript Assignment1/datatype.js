be52a552ed55b16d1de3cf3e9c10c8a30e89b50b
della-tech <85329240+della-tech@users.noreply.github.com>
2023-05-07 21:17:26 +0530

    Add files via upload

---
+++ Javascript Assignment1/datatype.js
@@ -0,0 +1,13 @@
+document.write("Name:Della Maria<br>") 
+document.write("Name:<em>Della Maria</em><br>")
+document.write( "City: Cherthala<br>")
+let hobbies=['Watching series','social media','Mobile games']
+    document.write(hobbies,"<br>")
+const num=22
+document.write("22 is a ",typeof num)
+var date= new Date()
+var day = date.getDate();
+var month = date.getMonth() + 1; //January is month 0
+var year = date.getFullYear();
+var newDate = day + "/" + month + "/" + year;
+document.write("<br>Date:",newDate)
